d796ff560109986d9eb660d67957d2882767d99b
Scott Winkler <scottjwinkler@gmail.com>
2019-11-21 16:55:40 -0800

    Update main.tf

--- chapter5/listing5.6/main.tf
+++ chapter5/listing5.6/main.tf
@@ -8,7 +8,6 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = "serverless"
-  resource_group_name   = azurerm_resource_group.default.name
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
