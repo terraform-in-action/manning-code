cca8048eab48224e577e85ad936540e1206f41aa
Scott Winkler <scottjwinkler@gmail.com>
2019-11-21 16:55:53 -0800

    Update main.tf

--- chapter5/listing5.7/main.tf
+++ chapter5/listing5.7/main.tf
@@ -4,7 +4,6 @@ module "ballroom" {
 
 resource "azurerm_storage_blob" "storage_blob" {
   name                   = "server.zip"
-  resource_group_name    = azurerm_resource_group.default.name
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = azurerm_storage_container.storage_container.name
   type                   = "block"
