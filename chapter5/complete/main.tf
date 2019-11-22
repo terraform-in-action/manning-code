834ac9cba3d0f3c7cc773e3714f4705c9255577a
Scott Winkler <scottjwinkler@gmail.com>
2019-11-21 16:54:33 -0800

    Update main.tf

--- chapter5/complete/main.tf
+++ chapter5/complete/main.tf
@@ -23,7 +23,6 @@ resource "azurerm_storage_account" "storage_account" {
 
 resource "azurerm_storage_container" "storage_container" {
   name                  = "serverless"
-  resource_group_name   = azurerm_resource_group.default.name
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 }
@@ -34,7 +33,6 @@ module "ballroom" {
 
 resource "azurerm_storage_blob" "storage_blob" {
   name                   = "server.zip"
-  resource_group_name    = azurerm_resource_group.default.name
   storage_account_name   = azurerm_storage_account.storage_account.name
   storage_container_name = azurerm_storage_container.storage_container.name
   type                   = "block"
