bde850725e798576ce7fd5d2d2f80098d80e8aa4
Scott Winkler <scottjwinkler@gmail.com>
2019-11-24 17:45:03 -0800

    Update outputs.tf

--- chapter6/part1_s3backend/outputs.tf
+++ chapter6/part1_s3backend/outputs.tf
@@ -1,7 +1,8 @@
 output "config" {
-    value = {
-      bucket = aws_s3_bucket.s3_bucket.bucket
-      region = data.aws_region.current.name
-      role_arn = aws_iam_role.iam_role.arn
-    }
+  value = {
+    bucket         = aws_s3_bucket.s3_bucket.bucket
+    region         = data.aws_region.current.name
+    role_arn       = aws_iam_role.iam_role.arn
+    dynamodb_table = aws_dynamodb_table.dynamodb_table.name
+  }
 }
