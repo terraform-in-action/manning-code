ce7ed11c9ec5e612bc96d37c710c81610d7f8974
Dustin Alandzes <5882512+DustinAlandzes@users.noreply.github.com>
2021-10-19 13:58:28 -0500

    update chapter 11 to use terraform-in-action instead of scottwinkler

--- chapter11/complete/terraform-provider-petstore/main.go
+++ chapter11/complete/terraform-provider-petstore/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-	"github.com/scottwinkler/terraform-provider-petstore/petstore"
+	"github.com/terraform-in-action/terraform-provider-petstore/petstore"
 )
 
 func main() {
